f17f05873500279a53d5f384cc726c27cb2f7cb1
Nomes27 <nomi_smith18@hotmail.com>
2020-11-02 10:27:36 +0000

    connect firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import firebase from "./config";
+//import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//NEED TO INSTALL BOTH FIREBASE AND FIREBASE TOOLS
+
+const db = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Test</header>
     </div>
   );
 }
