19e7a9c340978a4b44b7bd890da38fbb0f2cf657
aph88 <alexphunter@gmail.com>
2020-11-03 17:03:40 +0000

    add friends container for li and some basic css

--- src/Components/FriendsList.jsx
+++ src/Components/FriendsList.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function FriendsList(props) {
   return <ul className="friendslist">
     {props.friends.map(friend => {
-      return <li>{friend}</li>
+      return <div className="friendslist-friend"><li>{friend}</li></div>
     })}
           </ul>
 }
