0ad149f458e74f8ba39712d1daee601cf853d260
Nomes27 <nomi_smith18@hotmail.com>
2020-11-03 17:15:28 +0000

    basic structure and css for profile page

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import FriendsList from "./FriendsList";
+
+const user = {
+  name: "alex",
+  avatar:
+    "https://bigideasforsmallbusiness.com/wp-content/uploads/2017/07/blog4site_7-13-17_Quiz-on-Tax-Rules_dreamstime-480x420.jpg",
+};
 
 function ProfilePage() {
-  return <h1>Profile</h1>;
+  return (
+    <div>
+      <section>
+        <h1 className="profile_h1">{user.name.toUpperCase()}</h1>
+        <button className="profile_button">Change Username</button>
+      </section>
+      <section>
+        <img className="profile_avatar" src={`${user.avatar}`}></img>
+        <br />
+        <button className="profile_button">Change Avatar</button>
+      </section>
+      <FriendsList />
+      <form>
+        <label htmlFor="name">Add friend by username</label>
+        <br></br>
+        <input
+          className="profile_form"
+          type="text"
+          id="name"
+          name="name"
+        ></input>
+        <button className="profile_form">ADD</button>
+      </form>
+    </div>
+  );
 }
 
 export default ProfilePage;
