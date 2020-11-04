d1d978636fb4e038891c49789d07c618c744ea7e
tiaeastwood <teastwood88@gmail.com>
2020-11-04 12:35:23 +0000

    add classes to table data to make it easier ot acccess

--- src/Components/DashBoard/LeaderBoard.jsx
+++ src/Components/DashBoard/LeaderBoard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+//info from the db should fill the table
+//examples are in there for now, but the username and score should corresponde to the first 10 items of the leaderboard collection on firestore
+
 function LeaderBoard() {
   return <div className='leaderboard'>
   <h2 className='leaderboard--title'>Leaderboard</h2>
@@ -13,58 +16,60 @@ function LeaderBoard() {
     </thead>
     <tbody>
     <tr>
-        <td>1</td>
-        <td>ilikequizzes</td>
-        <td>500</td>
+        <td className='leaderboard--position'>1</td>
+        <td classname='leaderboard--username'>ilikequizzes</td>
+        <td className='leaderboard--score'>500</td>
     </tr>
     <tr>
-        <td>2</td>
-        <td>notacat</td>
-        <td>450</td>
+        <td className='leaderboard--position'>2</td>
+        <td classname='leaderboard--username'>notacat</td>
+        <td className='leaderboard--score'>450</td>
     </tr>
     <tr>
-        <td>3</td>
-        <td>coolbean</td>
-        <td>420</td>
+        <td className='leaderboard--position'>3</td>
+        <td classname='leaderboard--username'>coolbean</td>
+        <td className='leaderboard--score'>420</td>
     </tr>
     <tr>
-        <td>4</td>
-        <td>Quizmaster2020</td>
-        <td>400</td>
+        <td className='leaderboard--position'>4</td>
+        <td classname='leaderboard--username'>Quizmaster2020</td>
+        <td className='leaderboard--score'>400</td>
     </tr>
     <tr>
-        <td>5</td>
-        <td>Fredo</td>
-        <td>380</td>
+        <td className='leaderboard--position'>5</td>
+        <td classname='leaderboard--username'>Fredo</td>
+        <td className='leaderboard--score'>380</td>
     </tr>
     <tr>
-        <td>6</td>
-        <td>Sal10</td>
-        <td>340</td>
+        <td className='leaderboard--position'>6</td>
+        <td classname='leaderboard--username'>Sal10</td>
+        <td className='leaderboard--score'>340</td>
     </tr>
     <tr>
-        <td>7</td>
-        <td>BillyBones</td>
-        <td>340</td>
+        <td className='leaderboard--position'>7</td>
+        <td classname='leaderboard--username'>BillyBones</td>
+        <td className='leaderboard--score'>340</td>
     </tr>
     <tr>
-        <td>8</td>
-        <td>smartypants</td>
-        <td>300</td>
+        <td className='leaderboard--position'>8</td>
+        <td classname='leaderboard--username'>smartypants</td>
+        <td className='leaderboard--score'>300</td>
     </tr>
     <tr>
-        <td>9</td>
-        <td>jazzyapple</td>
-        <td>280</td>
+        <td className='leaderboard--position'>9</td>
+        <td classname='leaderboard--username'>jazzyapple</td>
+        <td className='leaderboard--score'>280</td>
     </tr>
     <tr>
-        <td>10</td>
-        <td>poppinfresh</td>
-        <td>200</td>
+        <td className='leaderboard--position'>10</td>
+        <td classname='leaderboard--username'>poppinfresh</td>
+        <td className='leaderboard--score'>200</td>
     </tr>
     </tbody>
 </table>
   </div>;
+
+
 }
 
 export default LeaderBoard;
