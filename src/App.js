f75e1ed33b9807f4f0f07e701c34d87ec220187e
Nomes27 <nomi_smith18@hotmail.com>
2020-11-04 16:57:01 +0000

    hooks versus classes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import firebase from "./config";
 import "firebase/firestore";
 import "firebase/auth";
 import LandingPage from "./Components/LandingPage/LandingPage";
+
+import React from "react";
 import { Router } from "@reach/router";
 import ProfilePage from "./Components/ProfilePage/ProfilePage";
 import Room from "./Components/QuizRoom/Room";
@@ -15,18 +17,34 @@ import DashBoard from "./Components/DashBoard/Dashboard";
 const db = firebase.firestore();
 const rooms = db.collection("rooms");
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <LandingPage path="/" />
-        <DashBoard path="/dashboard" />
-        <ProfilePage path="/profile" />
-        <Host path="/quiz" />
-        <Room path="quiz/:room_id" />
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: "test",
+  };
+
+  setUser = (user) => {
+    this.setState({ user: user });
+  };
+  componentDidUpdate() {
+    console.log("here is in app", this.state.user);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <LandingPage
+            path="/"
+            setUser={this.setUser}
+            testUser={this.state.user}
+          />
+          <DashBoard path="/dashboard" user={this.state.user} />
+          <ProfilePage path="/profile" />
+          <Host path="/quiz" />
+          <Room path="quiz/:room_id" />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,29 +1,45 @@
-import {useEffect, useState} from 'react';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
+import { useEffect, useState } from "react";
 
-const LandingPage = () => {
-    const [signUp, setSignUp] = useState(false);
+import React from "react";
 
-    const handleClick = (event) => {
-        setSignUp(!signUp);
-    }
-    
-    
-    console.log(signUp);
-    
-        return (
-            <div className="landing-page--wrapper">
-                <div className="landing-page--graphic">
-                    
-                </div>
-                <div className="landing-page--sign-in">
-                    {signUp ? <SignUp /> : <SignIn />}
-                    <button className="landing-page--sign-in--modal-btn" onClick={handleClick}>{signUp ? 'Back' : 'Sign Up'}</button>
-                </div>
-            </div>
-        )
-    
-}
+const LandingPage = (props) => {
+  const [user, setUser] = useState("");
+
+  const updateUsername = (event) => {
+    setUser(event.target.value);
+    console.log(user);
+  };
+  const submitUser = (event) => {
+    event.preventDefault();
+    //post req
+    console.log("clicked");
+  };
+
+  useEffect(() => {
+    console.log(props.testUser);
+  });
+  return (
+    <div className="landing-page--wrapper">
+      <div className="landing-page--graphic"></div>
+      <div className="landing-page--sign-in">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            props.setUser(user);
+            console.log(props.testUser);
+          }}
+        >
+          <input
+            onChange={updateUsername}
+            type="text"
+            placeholder="enter username..."
+          ></input>
+        </form>
+
+        <button type="submit">START PLAYING</button>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
