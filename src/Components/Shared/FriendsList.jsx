838fd4432e89f92dc36536ba483a46b01143420b
Nomes27 <nomi_smith18@hotmail.com>
2020-11-04 11:58:03 +0000

    friendsList integration

--- src/Components/Shared/FriendsList.jsx
+++ src/Components/Shared/FriendsList.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 
-const deleteFriend = (friend) => {
-  
-}
+const deleteFriend = (friend) => {};
 
 function FriendsList(props) {
-  return (<div className="friendslist">
-    <h3>Friends</h3>
-    {props.friends.map(friend => {
-      return <div className="friendslist-friend" key={friend}>
-        <p>{friend}</p>
-       {props.path === "/dashboard" &&  <p>Online</p>}
-       {props.path === "/profile" && <button onClick={(e) => {
-         e.preventDefault();
-         deleteFriend(friend)
-       }}>X</button>}
-       </div>
-    })}
-    </div>)
+  return (
+    <div className="friendslist">
+      <h3>Friends</h3>
+      {props.friends.map((friend) => {
+        return (
+          <div className="friendslist-friend" key={friend}>
+            <p>{friend}</p>
+            {props.path === "/dashboard" && <p>Online</p>}
+            {props.path === "/profile" && (
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  deleteFriend(friend);
+                }}
+              >
+                X
+              </button>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default FriendsList;
