292bda7bd8cdd0b20130caafd1249f13334d6613
tiaeastwood <teastwood88@gmail.com>
2020-11-04 11:17:49 +0000

    create leaderboard component

--- src/Components/DashBoard/LeaderBoard.jsx
+++ src/Components/DashBoard/LeaderBoard.jsx
@@ -1,7 +1,70 @@
 import React from "react";
 
 function LeaderBoard() {
-  return <h1>Leaderboard goes here...</h1>;
+  return <div className='leaderboard'>
+  <h2 className='leaderboard--title'>Leaderboard</h2>
+<table className='leaderboard--table'>
+    <thead>
+    <tr>
+        <th>Position</th>
+        <th>Username</th>
+        <th>Score</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>ilikequizzes</td>
+        <td>500</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>notacat</td>
+        <td>450</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>coolbean</td>
+        <td>420</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Quizmaster2020</td>
+        <td>400</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Fredo</td>
+        <td>380</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>Sal10</td>
+        <td>340</td>
+    </tr>
+    <tr>
+        <td>7</td>
+        <td>BillyBones</td>
+        <td>340</td>
+    </tr>
+    <tr>
+        <td>8</td>
+        <td>smartypants</td>
+        <td>300</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>jazzyapple</td>
+        <td>280</td>
+    </tr>
+    <tr>
+        <td>10</td>
+        <td>poppinfresh</td>
+        <td>200</td>
+    </tr>
+    </tbody>
+</table>
+  </div>;
 }
 
 export default LeaderBoard;
