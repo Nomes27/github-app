e1be7c597d1940db5ca241fdf2a9e325ff8598da
Nomes27 <nomi_smith18@hotmail.com>
2020-11-05 17:04:57 +0000

    create quiz room

--- src/Components/DashBoard/Dashboard.jsx
+++ src/Components/DashBoard/Dashboard.jsx
@@ -5,63 +5,78 @@ import { navigate } from "@reach/router";
 import firebase from "../../config";
 import "firebase/firestore";
 const db = firebase.firestore();
-const userDB = db.collection("users").doc("alexhunter");
+const rooms = db.collection("rooms");
 
 class DashBoard extends React.Component {
+  state = {
+    user: "",
+  };
 
-state = {
-  user: '',
-}
+  generateCode = () => {
+    return "NJHJ";
+  };
 
-generateCode = () => {
-  return "NJHJ"
-}
-updateHost = (event) => {
-  event.preventDefault()
-  this.props.setHost(true)
-  const room = this.generateCode()
-  navigate(`/quiz/${room}`)
-}
+  setUpRoom = (code) => {
+    rooms
+      .doc(code)
+      .set({
+        users: [{ username: this.props.user, score: 0, answers: [] }],
+        host: this.props.user,
+      })
+      .then(() => {
+        console.log();
+      });
+    // make the room doc(as generated code), puts in the active user into the room
+  }; //doing this here, so that users are available to view in host lobby
 
-// componentDidMount() {
-//   userDB
-//   .get()
-//   .then(function (doc) {
-//     if (doc.exists) {
-//       const user = doc.data();
-//       this.setState({
-//         user: user.name
-//       })
-//     } else {
-//       // doc.data() will be undefined in this case
-//       console.log("No such document!");
-//     }
-//   })
-//   .catch(function (error) {
-//     console.log("Error getting document:", error);
-//   });
-// }
+  updateHost = (event) => {
+    event.preventDefault();
+    this.props.setHost(true);
+    const room = this.generateCode();
+    this.setUpRoom(room);
+    navigate(`/quiz/${room}`);
+  };
 
- render() {
-  return (
-    <div>
-      <header className="dashboard_header">
-        <div className="dashboard_header_buttons">
-          <button className="dashboard_header_button">Log out </button>
-          <button className="dashboard_header_button">Settings </button>
-        </div>
-        <h1>Hello, {this.props.user}!</h1>
-      </header>
+  // componentDidMount() {
+  //   userDB
+  //   .get()
+  //   .then(function (doc) {
+  //     if (doc.exists) {
+  //       const user = doc.data();
+  //       this.setState({
+  //         user: user.name
+  //       })
+  //     } else {
+  //       // doc.data() will be undefined in this case
+  //       console.log("No such document!");
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error getting document:", error);
+  //   });
+  // }
+
+  render() {
+    return (
       <div>
-        <button className="dashboard_game_buttons" onClick={this.updateHost}>Host Game</button>
-        <button className="dashboard_game_buttons">Join Game</button>
+        <header className="dashboard_header">
+          <div className="dashboard_header_buttons">
+            <button className="dashboard_header_button">Log out </button>
+            <button className="dashboard_header_button">Settings </button>
+          </div>
+          <h1>Hello, {this.props.user}!</h1>
+        </header>
+        <div>
+          <button className="dashboard_game_buttons" onClick={this.updateHost}>
+            Host Game
+          </button>
+          <button className="dashboard_game_buttons">Join Game</button>
+        </div>
+        {/* <FriendsList friends={user.friends} path={props.path} /> */}
+        <LeaderBoard />
       </div>
-      {/* <FriendsList friends={user.friends} path={props.path} /> */}
-      <LeaderBoard />
-    </div>
-  );
- }
-
+    );
+  }
 }
 
 export default DashBoard;
